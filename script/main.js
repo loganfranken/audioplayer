52fc24459670acb9341e83028c5225e791aa7ee8
Logan Franken <loganfranken@gmail.com>
2015-11-17 07:48:45 -0800

    Moved functions around, audio player is now added to element

--- script/main.js
+++ script/main.js
@@ -6,6 +6,8 @@
     RightArrow: 39
   };
 
+  document.addEventListener('keydown', handleKeyDown, false);
+
   var players = document.getElementsByClassName('audio-player');
   var playerCount = players.length;
 
@@ -19,58 +21,60 @@
     // File Input
     var fileInput = document.createElement('input');
     fileInput.setAttribute('type', 'file');
-
+    fileInput.addEventListener('change', loadFile, false);
     player.appendChild(fileInput);
+
+    // Audio Player
+    var audioPlayer = document.createElement('audio');
+    audioPlayer.setAttribute('controls', '');
+    player.appendChild(audioPlayer);
+  }
+
+  function loadFile(fileInputEvent)
+  {
+    var file = fileInputEvent.target.files[0];
+
+    if(file)
+    {
+      var reader = new FileReader();
+
+      reader.onload = function(fileLoadEvent) {
+          var dataUrl = fileLoadEvent.currentTarget.result;
+          audioPlayer.setAttribute('src', dataUrl);
+
+          audioPlayer.focus();
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function handleKeyDown(keyDownEvent)
+  {
+    // Handle: Toggle Pause/Play
+    if(keyDownEvent.keyCode === KeyCode.Space)
+    {
+      if(audioPlayer.paused)
+      {
+        audioPlayer.play();
+      }
+      else
+      {
+        audioPlayer.pause();
+      }
+    }
+
+    // Handle: Rewind
+    if(keyDownEvent.keyCode === KeyCode.LeftArrow)
+    {
+      audioPlayer.currentTime--;
+    }
+
+    // Handle: Fast-Forward
+    if(keyDownEvent.keyCode === KeyCode.RightArrow)
+    {
+      audioPlayer.currentTime++;
+    }
   }
 
 })();
-
-function loadFile(fileInputEvent)
-{
-  var file = fileInputEvent.target.files[0];
-
-  if(file)
-  {
-    var reader = new FileReader();
-
-    reader.onload = function(fileLoadEvent) {
-        var dataUrl = fileLoadEvent.currentTarget.result;
-        audioPlayer.setAttribute('src', dataUrl);
-
-        audioPlayer.focus();
-    }
-
-    reader.readAsDataURL(file);
-  }
-}
-
-function handleKeyDown(keyDownEvent)
-{
-  // Handle: Toggle Pause/Play
-  if(keyDownEvent.keyCode === KeyCode.Space)
-  {
-    if(audioPlayer.paused)
-    {
-      audioPlayer.play();
-    }
-    else
-    {
-      audioPlayer.pause();
-    }
-  }
-
-  // Handle: Rewind
-  if(keyDownEvent.keyCode === KeyCode.LeftArrow)
-  {
-    audioPlayer.currentTime--;
-  }
-
-  // Handle: Fast-Forward
-  if(keyDownEvent.keyCode === KeyCode.RightArrow)
-  {
-    audioPlayer.currentTime++;
-  }
-}
-
-fileInput.addEventListener('change', loadFile, false);
-document.addEventListener('keydown', handleKeyDown, false);
