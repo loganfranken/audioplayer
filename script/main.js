c4e32b609f4ee070674e72f5e111bd3264123ac4
Logan Franken <loganfranken@gmail.com>
2015-12-13 13:46:53 -0800

    File can now be added using either drag and drop or file select

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 (function() {
 
+  var defaultStatusMessage = 'Drop a file here or choose one';
+
   var KeyCode = {
     Space: 32,
     LeftArrow: 37,
@@ -16,6 +18,11 @@
 
     var self = this;
 
+    // Status Message
+    this.statusMessageElem = document.createElement('div');
+    this.statusMessageElem.innerHTML = defaultStatusMessage;
+    this.playerElem.appendChild(this.statusMessageElem);
+
     // File Input
     this.fileInputElem = document.createElement('input');
     this.fileInputElem.setAttribute('type', 'file');
@@ -30,6 +37,12 @@
     // Keyboard Controls
     document.addEventListener('keydown', function() { self.onKeyDown.apply(self, arguments); }, false);
 
+    // Drag and Drop Events
+    this.playerElem.addEventListener('dragenter', function() { self.onDragEnter.apply(self, arguments); }, false);
+    this.playerElem.addEventListener('dragleave', function() { self.onDragLeave.apply(self, arguments); }, false);
+    this.playerElem.addEventListener('dragover', function() { self.onDragOver.apply(self, arguments); }, false);
+    this.playerElem.addEventListener('drop', function() { self.onDrop.apply(self, arguments); }, false);
+
   }
 
   AudioPlayer.prototype.onLoadFile = function(fileInputEvent) {
@@ -37,21 +50,7 @@
     var audioElem = this.audioElem;
 
     var file = fileInputEvent.target.files[0];
-
-    if(file)
-    {
-      var reader = new FileReader();
-
-      reader.onload = function(fileLoadEvent)
-      {
-          var dataUrl = fileLoadEvent.currentTarget.result;
-          audioElem.setAttribute('src', dataUrl);
-          audioElem.focus();
-          audioElem.play();
-      }
-
-      reader.readAsDataURL(file);
-    }
+    this.loadFile(file);
 
   }
 
@@ -80,6 +79,65 @@
     if(keyDownEvent.keyCode === KeyCode.RightArrow)
     {
       this.audioElem.currentTime++;
+    }
+
+  }
+
+  AudioPlayer.prototype.onDragEnter = function(dragEvent) {
+
+    dragEvent.stopPropagation();
+    dragEvent.preventDefault();
+
+  }
+
+  AudioPlayer.prototype.onDragLeave = function(dragEvent) {
+
+    dragEvent.stopPropagation();
+    dragEvent.preventDefault();
+
+    this.statusMessageElem.innerHTML = defaultStatusMessage;
+
+  }
+
+  AudioPlayer.prototype.onDragOver = function(dragEvent) {
+
+    dragEvent.stopPropagation();
+    dragEvent.preventDefault();
+
+    dragEvent.dataTransfer.dropEffect = 'copy';
+
+  }
+
+  AudioPlayer.prototype.onDrop = function(dropEvent) {
+
+    dropEvent.stopPropagation();
+    dropEvent.preventDefault();
+
+    // Update status message text
+    this.statusMessageElem.innerHTML = defaultStatusMessage;
+
+    // Load the file
+    this.loadFile(dropEvent.dataTransfer.files[0]);
+
+  }
+
+  AudioPlayer.prototype.loadFile = function(file) {
+
+    var audioElem = this.audioElem;
+
+    if(file)
+    {
+      var reader = new FileReader();
+
+      reader.onload = function(fileLoadEvent)
+      {
+          var dataUrl = fileLoadEvent.currentTarget.result;
+          audioElem.setAttribute('src', dataUrl);
+          audioElem.focus();
+          audioElem.play();
+      }
+
+      reader.readAsDataURL(file);
     }
 
   }
